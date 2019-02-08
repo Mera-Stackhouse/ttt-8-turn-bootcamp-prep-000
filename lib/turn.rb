49c5317423e615b3a50a3432e2f3cdf5a3455c5e
Mera Stackhouse <mera.stackhouse@gmail.com>
2019-02-08 23:34:04 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -37,11 +37,14 @@ def move(board, index, token = "X")
   puts token
 end  
 
-def turn 
+def turn(board)
   puts "Type 1-9 for where you want to go:"
   input = gets.strip
   index = input_to_index(input)
-  display_board()
+  if (valid_move?(board, index) == true)
+    move(board,index)
+    display_board(board)
+  end 
 end  
 
 
